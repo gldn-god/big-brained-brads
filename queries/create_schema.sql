7ba11a33cee8b70efd8df5a895832022f8c09e1f
KStrange89 <katie.lynn.strange.89@gmail.com>
2021-07-19 18:50:56 -0600

    adjusted etl-draft and sql

--- queries/create_schema.sql
+++ queries/create_schema.sql
@@ -1,8 +1,8 @@
 DROP TABLE IF EXISTS brain_weights;
 
 CREATE TABLE brain_weights (
-	"gender" int,
-	"age_range" int,
-	"head_size_cm3" int,
-	"weight_grams" int
+	"gender" varchar,
+	"age" varchar,
+	"size" int,
+	"weight" int
 )
